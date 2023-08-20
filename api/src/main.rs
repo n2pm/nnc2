dcbb2ab0fbec1a321ee6fcf73a891c5c5fe8fe7b
Honbra <honbra@honbra.com>
2023-08-21 00:49:22 +0200

    Auto-migrate on startup, fix and enhance logging

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,30 +2,38 @@ mod config;
 mod routing;
 mod tracing;
 
-use ::tracing::info;
+use ::tracing::{info, debug};
 use axum::Server;
 use eyre::Context;
+use nnc_migration::{Migrator, MigratorTrait};
 use sea_orm::Database;
 
 use crate::{config::Config, routing::make_router, tracing::set_up_tracing};
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    set_up_tracing("notnetcoin").context("Failed to set up tracing")?;
+    set_up_tracing("nnc_api").context("Failed to set up tracing")?;
 
+    debug!("Loading config");
     let Config {
         listen_addr,
         db_addr,
     } = Config::load().context("Failed to load config")?;
 
+    debug!("Connecting to database");
     let db = Database::connect(db_addr)
         .await
-        .context("Failed to connect to the database")?;
+        .context("Failed to connect to database")?;
 
+    debug!("Migrating database");
+    Migrator::up(&db, None)
+        .await
+        .context("Failed to migrate database")?;
+
+    debug!("Creating router");
     let app = make_router(db);
 
-    info!("Beginning to listen on {listen_addr}");
-
+    info!("Starting service on {listen_addr}");
     Server::bind(&listen_addr)
         .serve(app.into_make_service())
         .await
